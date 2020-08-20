9bcafae2463e496901160f15c67bd6ad17317be7
Joe Dillig <40437881+joe-at-cp@users.noreply.github.com>
2020-08-20 10:32:36 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ variable "VPC_Region" {
 }
 
 variable "Resource_Group" {
-  default     = "default"
+  default     = "Default"
   description = "The resource group that will be used when provisioning the Check Point VSI. If left unspecififed, the account's default resource group will be used."
 }
 
