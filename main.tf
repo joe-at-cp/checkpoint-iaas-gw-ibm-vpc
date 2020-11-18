053be99ae3510b6ac0b3cc76ed450446c6b4ec6a
Joe Dillig <40437881+joe-at-cp@users.noreply.github.com>
2020-11-18 12:16:59 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 ##############################################################################
-# IBM Cloud Provider
+# IBM Cloud Provider 1.13.1
 ##############################################################################
 
 provider "ibm" {
-  ibmcloud_api_key   = "${var.ibmcloud_api_key}"
-  generation         = 2
-  region             = "${var.VPC_Region}"
-  ibmcloud_timeout   = 300
-  resource_group     = "${var.Resource_Group}"
+  ibmcloud_api_key = var.ibmcloud_api_key
+  generation       = 2
+  region           = var.VPC_Region
+  ibmcloud_timeout = 300
+  resource_group   = var.Resource_Group
 }
 
 ##############################################################################
@@ -20,13 +20,8 @@ variable "VPC_Region" {
 }
 
 variable "Resource_Group" {
-  default     = "Default"
-  description = "The resource group that will be used when provisioning the Check Point VSI. If left unspecififed, the account's default resource group will be used."
-}
-
-variable "VPC_Zone" {
   default     = ""
-  description = "The zone where the VPC, networks, and Check Point VSI will be provisioned."
+  description = "The resource group that will be used when provisioning the Check Point VSI. If left unspecififed, the account's default resource group will be used."
 }
 
 variable "VPC_Name" {
@@ -34,12 +29,17 @@ variable "VPC_Name" {
   description = "The VPC where the Check Point VSI will be provisioned."
 }
 
-variable "Frontend_Subnet_ID" {
+variable "Management_Subnet_ID" {
+  default     = ""
+  description = "The ID of the Check Point management subnet."
+}
+
+variable "External_Subnet_ID" {
   default     = ""
   description = "The ID of the subnet that exists in front of the Check Point Security Gateway that will be provisioned (the 'external' network)."
 }
 
-variable "Backend_Subnet_ID" {
+variable "Internal_Subnet_ID" {
   default     = ""
   description = "The ID of the subnet that exists behind  the Check Point Security Gateway that will be provisioned (the 'internal' network)."
 }
@@ -57,11 +57,6 @@ variable "VNF_CP-GW_Instance" {
 variable "VNF_Security_Group" {
   default     = ""
   description = "The name of the security group assigned to the Check Point VSI."
-}
-
-variable "vnf_gw_instance_name" {
-  default     = "checkpoint-gateway"
-  description = "The name of your Checkpoint GW Virtual Server to be provisioned."
 }
 
 variable "VNF_Profile" {
@@ -98,56 +93,59 @@ variable "ibmcloud_api_key" {
 # Data block 
 ##############################################################################
 
+data "ibm_is_subnet" "cp_subnet0" {
+  identifier = var.Management_Subnet_ID
+}
+
 data "ibm_is_subnet" "cp_subnet1" {
-  identifier = "${var.Frontend_Subnet_ID}"
+  identifier = var.External_Subnet_ID
 }
 
 data "ibm_is_subnet" "cp_subnet2" {
-  identifier = "${var.Backend_Subnet_ID}"
+  identifier = var.Internal_Subnet_ID
 }
 
 data "ibm_is_image" "cp_gw_custom_image" {
-  name = "${ibm_is_image.cp_gw_custom_image.name}"
+  name = ibm_is_image.cp_gw_custom_image.name
 }
 
 data "ibm_is_ssh_key" "cp_ssh_pub_key" {
-  name = "${var.SSH_Key}"
+  name = var.SSH_Key
 }
 
 data "ibm_is_instance_profile" "vnf_profile" {
-  name = "${var.VNF_Profile}"
+  name = var.VNF_Profile
 }
 
 data "ibm_is_region" "region" {
-  name = "${var.VPC_Region}"
+  name = var.VPC_Region
 }
 
 data "ibm_is_vpc" "cp_vpc" {
-  name = "${var.VPC_Name}"
-}
-
-data "ibm_is_zone" "zone" {
-  name   = "${var.VPC_Zone}"
-  region = "${data.ibm_is_region.region.name}"
+  name = var.VPC_Name
 }
 
 data "ibm_resource_group" "rg" {
-  name = "${var.Resource_Group}"
+  name = var.Resource_Group
 }
 
 ##############################################################################
 # Create Custom Image
 ##############################################################################
 
+# Generating random ID
+resource "random_uuid" "test" { }
+
 locals {
-  image_url    = "cos://${var.VPC_Region}/checkpoint-${var.VPC_Region}/Check_Point_${var.CP_Version}_Cloudguard_Security_Gateway.qcow2"
+  image_url = "cos://${var.VPC_Region}/checkpoint-${var.VPC_Region}/Check_Point_${var.CP_Version}_Cloudguard_Security_Gateway.qcow2"
 }
 
 resource "ibm_is_image" "cp_gw_custom_image" {
-  href             = "${local.image_url}"
-  name             = "${var.VNF_CP-GW_Instance}"
+  depends_on       = [random_uuid.test]
+  href             = local.image_url
+  name             = "${var.VNF_CP-GW_Instance}-${substr(random_uuid.test.result,0,8)}"
   operating_system = "centos-7-amd64"
-  resource_group   = "${data.ibm_resource_group.rg.id}"
+  resource_group   = data.ibm_resource_group.rg.id
 
   timeouts {
     create = "30m"
@@ -160,23 +158,23 @@ resource "ibm_is_image" "cp_gw_custom_image" {
 ##############################################################################
 
 resource "ibm_is_security_group" "ckp_security_group" {
-    name = "${var.VNF_Security_Group}"
-    vpc = "${data.ibm_is_vpc.cp_vpc.id}"
-    resource_group = "${data.ibm_resource_group.rg.id}"
+  name           = var.VNF_Security_Group
+  vpc            = data.ibm_is_vpc.cp_vpc.id
+  resource_group = data.ibm_resource_group.rg.id
 }
 
 #Egress All Ports
 resource "ibm_is_security_group_rule" "allow_egress_all" {
-  depends_on = ["ibm_is_security_group.ckp_security_group"]
-  group      = "${ibm_is_security_group.ckp_security_group.id}"
+  depends_on = [ibm_is_security_group.ckp_security_group]
+  group      = ibm_is_security_group.ckp_security_group.id
   direction  = "outbound"
   remote     = "0.0.0.0/0"
 }
 
 #Ingress All Ports
 resource "ibm_is_security_group_rule" "allow_ingress_all" {
-  depends_on = ["ibm_is_security_group.ckp_security_group"]
-  group      = "${ibm_is_security_group.ckp_security_group.id}"
+  depends_on = [ibm_is_security_group.ckp_security_group]
+  group      = ibm_is_security_group.ckp_security_group.id
   direction  = "inbound"
   remote     = "0.0.0.0/0"
 }
@@ -187,29 +185,39 @@ resource "ibm_is_security_group_rule" "allow_ingress_all" {
 ##############################################################################
 
 resource "ibm_is_instance" "cp_gw_vsi" {
-  depends_on = ["ibm_is_security_group_rule.allow_ingress_all", "data.ibm_is_image.cp_gw_custom_image"]
-  name    = "${var.vnf_gw_instance_name}"
-  image   = "${data.ibm_is_image.cp_gw_custom_image.id}"
-  profile = "${data.ibm_is_instance_profile.vnf_profile.name}"
-  resource_group = "${data.ibm_resource_group.rg.id}"
+  depends_on     = [ibm_is_security_group_rule.allow_ingress_all, data.ibm_is_image.cp_gw_custom_image]
+  name           = var.VNF_CP-GW_Instance
+  image          = ibm_is_image.cp_gw_custom_image.id
+  profile        = data.ibm_is_instance_profile.vnf_profile.name
+  resource_group = data.ibm_resource_group.rg.id
 
+  #eth0 - Management Interface
   primary_network_interface {
-    name   = "eth0"
-    subnet = "${data.ibm_is_subnet.cp_subnet1.id}"
-    security_groups = ["${ibm_is_security_group.ckp_security_group.id}"]
+    name            = "eth0"
+    subnet          = data.ibm_is_subnet.cp_subnet0.id
+    security_groups = [ibm_is_security_group.ckp_security_group.id]
   }
 
+  #eth1 - External Interface
   network_interfaces {
-    name   = "eth1"
-    subnet = "${data.ibm_is_subnet.cp_subnet2.id}"
-    security_groups = ["${ibm_is_security_group.ckp_security_group.id}"]
+    name            = "eth1"
+    subnet          = data.ibm_is_subnet.cp_subnet1.id
+    security_groups = [ibm_is_security_group.ckp_security_group.id]
   }
 
-  vpc  = "${data.ibm_is_vpc.cp_vpc.id}"
-  zone = "${data.ibm_is_zone.zone.name}"
-  keys = ["${data.ibm_is_ssh_key.cp_ssh_pub_key.id}"]
+  #eth2 - Internal Interface
+  network_interfaces {
+    name            = "eth2"
+    subnet          = data.ibm_is_subnet.cp_subnet2.id
+    security_groups = [ibm_is_security_group.ckp_security_group.id]
+  }
 
-  # user_data = "$(replace(file("cp-userdata.sh"), "cp-LICENSE-REPLACEMENT", var.vnf_license)"
+  vpc  = data.ibm_is_vpc.cp_vpc.id
+  zone = data.ibm_is_subnet.cp_subnet0.zone
+  keys = [data.ibm_is_ssh_key.cp_ssh_pub_key.id]
+
+  #Custom UserData
+  user_data = file("user_data")
 
   timeouts {
     create = "15m"
@@ -223,15 +231,15 @@ resource "ibm_is_instance" "cp_gw_vsi" {
 
 # Delete checkpoint firewall custom image from the local user after VSI creation.
 data "external" "delete_custom_image1" {
-  depends_on = ["ibm_is_instance.cp_gw_vsi"]
+  depends_on = [ibm_is_instance.cp_gw_vsi]
   program    = ["bash", "${path.module}/scripts/delete_custom_image.sh"]
 
   query = {
-    custom_image_id   = "${data.ibm_is_image.cp_gw_custom_image.id}"
-    region            = "${var.VPC_Region}"
+    custom_image_id = data.ibm_is_image.cp_gw_custom_image.id
+    region          = var.VPC_Region
   }
 }
 
 output "delete_custom_image1" {
-  value = "${lookup(data.external.delete_custom_image1.result, "custom_image_id")}"
+  value = lookup(data.external.delete_custom_image1.result, "custom_image_id")
 }
