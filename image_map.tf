a01647bc8f9a9866020aafacbc24cda29892e3d0
Joe Dillig <40437881+joe-at-cp@users.noreply.github.com>
2021-02-08 15:00:45 -0600

    Create image_map.tf

---
+++ image_map.tf
@@ -0,0 +1,49 @@
+locals {
+  image_map = {
+    
+    R81-Management = {
+      "us-south" = "r006-de1709f0-ecdf-40be-96b3-bc1f386c95c3"
+      "us-east"  = "r014-65537525-9808-4406-a89e-8652839eac25"
+      "eu-gb"    = "r018-d4a7790b-7b2d-404b-a1e3-d2076ae562bf"
+      "eu-de"    = "r010-197032fe-10fe-424a-9035-839009caa81a"
+      "jp-tok"   = "r022-c5563bc0-e6be-4b15-92be-1cc5b55d9a73"
+      "eu-fr2"   = "r030-29219b83-3216-4af5-bc87-ab29fb38d1c9"
+      "jp-osa"   = "r034-cf3de7a6-50fc-4647-8ff5-f2543104b80f"
+      "au-syd"   = "r026-ec1f6c7c-84b0-4811-9b69-aa6ff8d530cd"
+    }
+
+    R81-Gateway = {
+      "us-south" = "r006-3faf24da-b274-45b3-b4a7-7b439ada9c3e"
+      "us-east"  = "r014-5b6f53da-28e5-4c06-959d-33ab1e272862"
+      "eu-gb"    = "r018-185e198a-70f0-401b-8a74-3d68b6b30aa4"
+      "eu-de"    = "r010-0d00f463-5bba-4a42-a0d6-6cbf51c8c44e"
+      "jp-tok"   = "r022-71d0b1bf-2a0b-4da7-8523-9ffdd0a6154b"
+      "eu-fr2"   = "r030-57c452d3-b0e1-4dfd-897d-ff7ce1d79077"
+      "jp-osa"   = "r034-7a9acb18-3a08-461a-b85c-2cbe818ceed4"
+      "au-syd"   = "r026-8227d7e8-83b2-4e98-81aa-e602ef97e423"
+    }
+
+    R8040-Management = {
+      "us-south" = "r006-5538c037-86ee-412c-8f31-f98772ad65d8"
+      "us-east"  = "r014-7b0575f8-20a9-4832-b70f-29b2a6de6a7a"
+      "eu-gb"    = "r018-5750a248-0b2a-44c6-9433-c6ddf96674b7"
+      "eu-de"    = "r010-7c7b39c2-3fda-4fa6-8d50-32ac3c843783"
+      "jp-tok"   = "r022-fa9c12db-491f-40eb-bfd5-e8906d0f20ff"
+      "eu-fr2"   = "r010-7c7b39c2-3fda-4fa6-8d50-32ac3c843783"
+      "jp-osa"   = "r034-10dac24d-c85d-4d2d-8c9d-36f89459cccb"
+      "au-syd"   = "r026-f3b65e0d-99a3-49df-8923-7d4a0bece5aa"
+    }
+
+    R8040-Gateway = {
+      "us-south" = "r006-1849dbd7-db85-42c0-91a8-826be1bb598b"
+      "us-east"  = "r014-b653fee1-f0d7-468b-8c17-820184671d3b"
+      "eu-gb"    = "r018-f9d711a0-d719-4da5-83a1-b56bcb0b4d26"
+      "eu-de"    = "r010-ea21bfbd-3602-4a61-bf51-96f29aaea5d3"
+      "jp-tok"   = "r022-c08fe518-dfa7-4ee2-9a1c-c159f3ac2c5d"
+      "eu-fr2"   = "r030-9b9a98a2-0320-437e-81ad-0b2da02f9f99"
+      "jp-osa"   = "r034-18565597-efa8-4b16-b223-a3fe24f27f45"
+      "au-syd"   = "r026-ac4041b5-ef43-4517-82ac-88c6e583cc2f"
+    }
+  
+  }
+}
